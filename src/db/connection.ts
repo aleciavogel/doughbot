966ec0d68b46a4eb5faf2b51e4edd251192d6bcd
Alecia Vogel <hello@alecia.ca>
2023-03-08 07:31:49 -0700

    Hopefully fix auth issues

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,9 +2,10 @@ import {createConnection} from "mysql";
 
 const connection = createConnection({
   host: process.env.DB_HOST,
-  user: "root",
-  password: "my_password",
-  database: "discord_dev",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT ?? "3306") || 3306,
 });
 
 export default connection;
